dffcedb5432a97f36c748997270181d3777e9d22
adityayusuf12 <ayraharja88@gmail.com>
2019-11-20 10:49:58 +0700

    listenner fragment

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -3,14 +3,17 @@ package id.ac.polinema.idealbodyweight;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import id.ac.polinema.idealbodyweight.Fragment.AboutFragment;
+import id.ac.polinema.idealbodyweight.Fragment.MenuFragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity implements
+		MenuFragment.OnFragmentInteractionListener{
 
 	// Deklarasikan atribut Fragment di sini
+	MenuFragment menuFragment;
 	private AboutFragment aboutFragment;
 
 	@Override
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		aboutFragment = aboutFragment.newInstance("Aditya Yusuf Raharja");
+		menuFragment= new  MenuFragment();
+		getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, menuFragment)
+				.commit();
 	}
 
 	@Override
